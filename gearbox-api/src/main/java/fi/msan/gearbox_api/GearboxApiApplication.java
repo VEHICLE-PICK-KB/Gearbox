c9c8c654ebd00b09c65bcaa8dfcaf36dab2fb705
VEHICLE-PICK-KB <bgz828@myy.haaga-helia.fi>
2025-12-01 22:13:16 +0200

    Project initialization

---
+++ gearbox-api/src/main/java/fi/msan/gearbox_api/GearboxApiApplication.java
@@ -0,0 +1,13 @@
+package fi.msan.gearbox_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearboxApiApplication.class, args);
+	}
+
+}
---
+++ gearbox-api/src/test/java/fi/msan/gearbox_api/GearboxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.msan.gearbox_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearboxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
